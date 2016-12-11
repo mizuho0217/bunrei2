0999658d54362c53610c69adbf69f259dc1430c6
今津　瑞穂 <mizurinrinrin3@yahoo.co.jp>
2016-12-11 18:36:06 +0900

    ユーザーがプロフィールを編集できるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,9 @@ class UsersController < ApplicationController
   def update
     current_user.update(update_params)
   end
+
+  private
+  def update_params
+    params.require(:user).permit(:name, :email)
+  end
 end
